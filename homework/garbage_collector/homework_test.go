c69a863822477cde46af05908d03473670b5aedc
gromov.maksim9 <gromov.maksim9@wb.ru>
2025-06-07 21:14:41 +0200

    HW-11 implemented

--- homework/garbage_collector/homework_test.go
+++ homework/garbage_collector/homework_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 	"unsafe"
 
@@ -11,8 +10,30 @@ import (
 // go test -v homework_test.go
 
 func Trace(stacks [][]uintptr) []uintptr {
-	// need to implement
-	return nil
+	var result []uintptr
+	visited := make(map[uintptr]struct{})
+
+	for _, stack := range stacks {
+		for _, ptr := range stack {
+			trace(ptr, visited, &result)
+		}
+	}
+
+	return result
+}
+
+// trace - обход через DFS
+func trace(ptr uintptr, visited map[uintptr]struct{}, result *[]uintptr) {
+	if ptr == 0 {
+		return
+	}
+	if _, ok := visited[ptr]; ok {
+		return
+	}
+
+	visited[ptr] = struct{}{}
+	*result = append(*result, ptr)
+	trace(*(*uintptr)(unsafe.Pointer(ptr)), visited, result)
 }
 
 func TestTrace(t *testing.T) {
@@ -49,14 +70,15 @@ func TestTrace(t *testing.T) {
 	pointers := Trace(stacks)
 	expectedPointers := []uintptr{
 		uintptr(unsafe.Pointer(&heapPointer1)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
 		uintptr(unsafe.Pointer(&heapObjects[0])),
 		uintptr(unsafe.Pointer(&heapPointer2)),
-		uintptr(unsafe.Pointer(&heapObjects[1])),
 		uintptr(unsafe.Pointer(&heapObjects[2])),
 		uintptr(unsafe.Pointer(&heapPointer4)),
 		uintptr(unsafe.Pointer(&heapPointer3)),
 		uintptr(unsafe.Pointer(&heapObjects[3])),
 	}
 
-	assert.True(t, reflect.DeepEqual(expectedPointers, pointers))
+	assert.True(t, len(expectedPointers) == len(pointers))
+	assert.ElementsMatch(t, expectedPointers, pointers)
 }
